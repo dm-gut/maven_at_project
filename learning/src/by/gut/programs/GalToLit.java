fbcae3052073ef7595f3821b90ab6730dab5d446
d-gut-MM <dmitry.gut@moneyman.ru>
2020-05-17 18:30:20 +0300

    1.1 GalToLit

---
+++ learning/src/by/gut/programs/GalToLit.java
@@ -0,0 +1,11 @@
+package by.gut.programs;
+
+class GalToLit {
+    public static void main(String[] args) {
+        double gallons, litres;
+        gallons = 10;
+        litres = gallons * 3.7854;
+
+        System.out.println(gallons + " галлонам соответствует " + litres + " литра(-ов)");
+    }
+}
